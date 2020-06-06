0311ead7c0da7e1593cf21e1fb2d4df2b00bf4ce
diego_neri <diego@diegoneri.com>
2020-06-05 22:12:19 -0300

    Cria o programa NomeASCIIArt 1.0
    Adiciona a biblioteca Figgle
    
    Finaliza o programa com a fonte Standard do Figgle
    
    Atualiza o exemplo final de saída

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Figgle;
+
+namespace NomeASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string nomeTextao;
+            
+            Console.Write("Digite seu nome...: ");
+            
+            nome = Console.ReadLine();           
+            nomeTextao = FiggleFonts.Standard.Render(nome);
+
+            Console.WriteLine(nomeTextao);
+
+        }
+    }
+}
